8066fc3c29b510010b0a7f2072b5addbb5721bfd
Francesco Paparatto <francescopaparatto@gmail.com>
2026-02-10 13:03:52 +0100

    Create headers file with functions prototypes, types definitions, error codes

---
+++ src/include/commands.h
@@ -0,0 +1,8 @@
+#ifndef CGIT_COMMANDS_H
+#define CGIT_COMMANDS_H
+
+int handle_init(int argc, char *argv[]);
+int handle_cat_file(int argc, char *argv[]);
+int handle_hash_object(int argc, char *argv[]);
+
+#endif
---
+++ src/include/core.h
@@ -0,0 +1,30 @@
+#ifndef CGIT_CORE_H
+#define CGIT_CORE_H
+
+#include "common.h"
+
+typedef struct {
+  char *type;
+  size_t size;
+  unsigned char *data;
+} git_object_t;
+
+cgit_error_t read_object(const char *hash, git_object_t *obj);
+cgit_error_t write_object(const unsigned char *data, size_t len,
+                          const char *type, char *hash_out);
+void free_object(git_object_t *obj);
+
+cgit_error_t compress_data(const unsigned char *input, size_t input_len,
+                           buffer_t *output);
+cgit_error_t decompress_data(const unsigned char *input, size_t input_len,
+                             buffer_t *output);
+
+cgit_error_t compute_sha1(const unsigned char *data, size_t len, char *hex_out);
+
+cgit_error_t build_object_path(const char *hash, char *path_out,
+                               size_t path_size);
+cgit_error_t read_file(const char *path, buffer_t *output);
+int is_valid_hash(const char *hash);
+void buffer_free(buffer_t *buf);
+
+#endif
